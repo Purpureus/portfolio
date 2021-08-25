68dfd9084fcba67643aea0ae1a50ca30a5c1033f
Purpureus <purpureusdesign@gmail.com>
2021-08-25 15:11:37 +0200

    Added descriptions

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+const SERVER_URL = document.getElementById("server-url").href
+
 function log(...msg) {
 	console.log(...msg)
 }
@@ -6,39 +8,102 @@ function toArray(list) {
 	return Array.prototype.slice.call(list)
 }
 
+const body = document.querySelector("body")
 const videoPlayerContainer = document.getElementById("video-player-container")
 const videoPlayer = document.getElementById("video-player")
 const videoPlayerCloseBtn = document.getElementById("video-player-close-button")
+
+const readerContainer = document.getElementById("reader-container")
+const reader = document.getElementById("reader")
+const readerCloseBtn = document.getElementById("reader-close-button")
+
 const projects = toArray(document.getElementsByClassName("project"))
 
-videoPlayerContainer.addEventListener("click", () => {
+function loadHtmlToReader(url) {
+	fetch(url)
+		.then(res => {
+			return res.text()
+		})
+		.then(html => {
+			reader.innerHTML = html
+		})
+		.catch(error => {
+			console.log(error)
+		})
+}
+
+function openPlayer(url) {
+	body.classList.toggle('noscroll', true)
+	videoPlayerContainer.classList.remove("hidden")
+	videoPlayer.src = url
+}
+function closePlayer() {
+	body.classList.toggle('noscroll', false)
 	videoPlayerContainer.classList.add("hidden")
+}
+
+function openReader(url) {
+	body.classList.toggle('noscroll', true)
+	readerContainer.classList.remove("hidden")
+	loadHtmlToReader(url)
+}
+function closeReader() {
+	body.classList.toggle('noscroll', false)
+	readerContainer.classList.add("hidden")
+}
+
+videoPlayerContainer.addEventListener("click", () => {
+	closePlayer()
+})
+readerContainer.addEventListener("click", () => {
+	closeReader()
+})
+reader.addEventListener("click", (e) => {
+	e.stopPropagation()
 })
 
 function handleEscKey(e = null) {
 	if (e.key === "Escape") {
 		if (!videoPlayerContainer.classList.contains("hidden")) {
-			videoPlayerContainer.classList.add("hidden")
+			closePlayer()
+		}
+		if (!readerContainer.classList.contains("hidden")) {
+			closeReader()
 		}
 	}
 }
 document.addEventListener("keydown", handleEscKey)
-videoPlayer.addEventListener("keydown", () => {
-	log("PAPITAS")
-})
 
 projects.forEach((project) => {
 	const videoButton = project.querySelector(".video-button")
-	if (!videoButton) return
-	const url = videoButton.dataset.url
-	if (!url) return
-	videoButton.addEventListener("click", (e) => {
-		e.preventDefault()
-		videoPlayerContainer.classList.remove("hidden")
-		videoPlayer.src = url
-	})
+	if (videoButton) {
+		const url = videoButton.dataset.url
+		if (url) {
+			videoButton.addEventListener("click", (e) => {
+				e.preventDefault()
+				openPlayer(url)
+			})
+		}
+	}
+
+	const infoButton = project.querySelector(".info-button")
+	if (infoButton) {
+		const url = `${SERVER_URL}/${infoButton.dataset.url}`
+		if (url) {
+			infoButton.addEventListener("click", (e) => {
+				e.preventDefault()
+				openReader(url)
+			})
+		}
+	}
 })
 
 videoPlayerCloseBtn.addEventListener("click", () => {
-	videoPlayerContainer.classList.add("hidden")
+	closePlayer()
 })
+
+readerCloseBtn.addEventListener("click", () => {
+	closeReader()
+})
+
+// Project info
